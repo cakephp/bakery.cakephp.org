8ba7737c97654ae5425ddd2bd771504eb68fc143
Mark Story <mark@mark-story.com>
2019-05-16 12:16:52 -0400

    Fix print

--- server.py
+++ server.py
@@ -45,7 +45,7 @@ def main(HandlerClass=RootedHTTPRequestHandler, ServerClass=RootedHTTPServer):
     httpd = ServerClass(args.dir, server_address, HandlerClass)
 
     sa = httpd.socket.getsockname()
-    print "Serving HTTP on", sa[0], "port", sa[1], "..."
+    print("Serving HTTP on", sa[0], "port", sa[1], "...")
     httpd.serve_forever()
 
 
